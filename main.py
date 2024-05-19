15fe9cf3ce3e509cb1f682b641f960ef487fdc85
Goody <gcypanto@gmail.com>
2024-05-20 00:33:57 +0800

    shitposting implementation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord import app_commands, Interaction
+from discord import app_commands, Interaction, Message, utils
 from discord.ext import commands
 import os
 from os.path import join, dirname
@@ -12,7 +12,7 @@ TOKEN = os.environ.get("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True  # Ensure this intent is enabled for fetching messages
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 tree = bot.tree  # Use the bot's command tree
 
 
@@ -50,5 +50,41 @@ async def first_command(interaction: Interaction, msg: str = None):
             await interaction.followup.send("Invalid message ID.", ephemeral=True)
     else:
         await interaction.followup.send("🤓 ☝️", ephemeral=True)
+
+
+@bot.event
+async def on_message(ctx:Message):
+        # we do not want the bot to reply to itself
+        if ctx.author.bot:
+            return
+        
+        if ctx.content.startswith('$nerd'):
+            if ctx.reference != None:
+                message = await fetch_message_in_channel(ctx.channel,ctx.reference.message_id)
+                # webhook = await message.channel.create_webhook(name="gay")
+
+                # ret = await webhook.send("🤓 ☝️",username=ctx.author.name,avatar_url=ctx.author.avatar.url,wait=True)
+                await message.reply("🤓 ☝️", mention_author=False)
+                await ctx.delete()
+            else:
+                await ctx.reply('Hello!', mention_author=True)
+        
+        if ctx.content.__contains__('twitter.com') and not ctx.content.__contains__('fxtwitter'):
+            webhook = await ctx.channel.create_webhook(name="gayest")
+            reply = ctx.content.replace("twitter","fxtwitter")
+            await webhook.send(reply,username=ctx.author.name,avatar_url=ctx.author.avatar.url)
+            await ctx.delete()
+
+        if ctx.content.__contains__('x.com') and not ctx.content.__contains__('fxtwitter'):
+            webhook = await ctx.channel.create_webhook(name="gayest")
+            reply = ctx.content.replace("x","fxtwitter")
+            await webhook.send(reply,username=ctx.author.name,avatar_url=ctx.author.avatar.url)
+            await ctx.delete()
+
+        if ctx.content.__contains__('instagram.com') and not ctx.content.__contains__('ddinstagram'):
+            webhook = await ctx.channel.create_webhook(name="gayer")
+            reply = ctx.content.replace("instagram","ddinstagram")
+            await webhook.send(reply,username=ctx.author.name,avatar_url=ctx.author.avatar.url)
+            await ctx.delete()
 
 bot.run(TOKEN)
